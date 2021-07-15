9e38850137ab0a805d39531c8e94cfb13d66ba55
Harsh5chal <75268993+Harsh5chal@users.noreply.github.com>
2021-07-15 20:19:35 +0530

    ok

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"harsh5chal12345@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
